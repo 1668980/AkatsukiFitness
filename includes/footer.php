27093b756e4b308ea4edd62e6e0a08084cf5a465
1668980 <56259470+1668980@users.noreply.github.com>
2022-01-30 02:25:08 -0500

    Ajustement Header et Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,8 @@
-            <footer id="footer">
-                <p>
-                    <small>
-                        <?php echo 'Copyright &copy; ' . date("Y") . ' Akatsuki Fitness' ?>
-                    </small>
-                </p>
-            </footer>
-        </div>
-        <script src="js/app.js"></script>
-    </body>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" crossorigin="anonymous">
-    </script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-                integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-</html>
+    <footer id="footer">
+        <p>
+            <small>
+                <?php echo 'Copyright &copy; ' . date("Y") . ' Akatsuki Fitness' ?>
+            </small>
+        </p>
+    </footer>
+    </html>
--- includes/header.php
+++ includes/header.php
@@ -7,19 +7,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Akatuski : Fitness Manager</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="utilitaires/bootstrap-5.1.3-dist/css/bootstrap.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="css/style.css" />
+    <!-- Javascript -->
+    <script src="utilitaires/popper.min.js"></script>
+    <script src="utilitaires/jquery-3.6.0.min.js"></script>
+    <script src="utilitaires/bootstrap-5.1.3-dist/js/bootstrap.js"></script>
+    <script src="js/app.js"></script>
 </head>
-
-<body>
-    <nav style="background-color:red, height:15px">
-        <a href="#" class="mt-1">Akatsuki</a>
-        <a href="#" class="mt-1">Accueil</a>
-        <a href="login.php" class="mt-1">Se connecter</a>
-        <a href="signup.php" class="mt-1">S'enregistrer</a>
-    </nav>
-    <div class="container">
