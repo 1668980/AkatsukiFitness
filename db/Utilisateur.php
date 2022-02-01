14ca63201dc5cd97e1764f160e1131f2643fd05e
OrlandoAriel <orlandovillanueva11@gmail.com>
2022-02-01 17:01:44 -0500

    update class Utilisateur

--- db/Utilisateur.php
+++ db/Utilisateur.php
@@ -1,5 +1,18 @@
 <?php
 class Utilisateur {
-    
+    public $idUser;
+    public $email;
+    public $firstName;
+    public $lastName;
+    public $date;
+
+    public function __construct($id,$email,$first,$last,$date)
+    {
+        $this->idUser = $id;
+        $this->email = $email;
+        $this->firstName = $first;
+        $this->lastName = $last;
+        $this->date = $date;
+    }
 } 
 ?>
