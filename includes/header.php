18ed7cf39628ab0132a987dac324f9f529844274
Chha Saviet <phangchaly@gmail.com>
2022-02-02 02:43:02 -0500

    add error reporting

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,14 @@
 <!-- This session file contains code that starts/resumes a session -->
 <!-- In the header so it is included on every page, allowing session capability to be used on every page -->
 <?php include_once 'includes/session.php' ?>
+
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
