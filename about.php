0e0ef0fe2ee1d74ffe11b0cdb33ba04af3a6dfe4
EchoBhop <invisiblesteve3000@gmail.com>
2022-02-04 15:42:57 -0500

    Add files via upload
    page à propos, contenu finalisé. reste à rendre plus beau avec css

--- about.php
+++ about.php
@@ -7,11 +7,35 @@ require_once('includes/header.php');
 
     <div class="container w-50 ">
         <div class="card-group container-fluid ">
-
             <div class="card align-items-center mt-5 mb-5">
-                A propos de nous.
+                <h1>Notre équipe</h1>
             </div>
-           
+        </div>
+
+    <div class="card-group container-fluid ">
+
+        <div class="card align-items-center mt-5 mb-5">
+            <img src="images/blogs/b_01.jpg" alt="..." width="450" length="450">
+        </div>
+
+        <div class="card align-items-center mt-5 mb-5">
+            <h3>En 2020, la pandemie nous a tous frappé. Beaucoup on dût arrêter de s'entrainer à cause de la
+                fermeture des places d'entrainement. Donc, nous voulons rendre l'entrainement à la maison plus accessible et facile.
+            </h3>
+        </div>
+    </div>
+
+    <div class="card-group container-fluid ">
+        <div class="card align-items-center mt-5 mb-5">
+            <h3>Lancé en 2022, notre équipe est constituée de 6 programmeurs motivés 
+            à se remettre en forme, tout comme vous et nous voulons insiter les autres à l'entrainement. 
+            Nous vous offrons un système simple d'utilisation, mais très efficace. Avec notre suivi de poids
+            et de statistique, vous alez atteindre votre but avec facilité et motivation.
+            </h3>  
+        </div>
+        
+        <div class="card align-items-center mt-5 mb-5">
+            <img src="images/about/p_team01.jpeg" alt="..." width="450" length="300">
         </div>
     </div>
 </main>
