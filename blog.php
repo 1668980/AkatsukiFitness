9303e829445511bdc9e1e55a18ba26c6d1433e9b
0rus <201668980@collegeahuntsic.qc.ca>
2022-02-05 00:33:10 -0500

    Création des cartes pour lister les blogs

--- blog.php
+++ blog.php
@@ -1,34 +1,60 @@
 <?php
 require_once('includes/header.php');
 ?>
-
-<main class="bg-dark">
-    <div class="container w-50 ">
-        <div class="card-group container-fluid ">
-
-            <!-- TODO: foreach articles du blog -->
-
-
-            <div class="card align-items-center mt-5 mb-5">
-                article 1
-                <a href="blog_article.php?id=1">show article</a>=
+<div class="container mt-5">
+    <div class="card-group container-fluid">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                    </div>
+                </div>
             </div>
-            
-            <div class="card align-items-center mt-5 mb-5">
-                article 2 
-                <a href="blog_article.php?id=2">show article</a>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                    <div class="card-body" style="max-width: 540px; max-height: 240px ">
+                        <h4 class="card-title"> Lutter contre les bras flasque</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Exercice </h6>
+                    </div>
+                </div>
             </div>
-
-            <div class="card align-items-center mt-5 mb-5">
-                article 3
-                <a href="blog_article.php?id=3">show article</a>
-
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                    <div class="card-body" style="max-width: 540px; max-height: 240px ">
+                        <h4 class="card-title"> Le sport en 2 coup 3 temps </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Entrainement </h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                    <div class="card-body" style="max-width: 540px; max-height: 240px ">
+                        <h4 class="card-title"> Le parcours à suivre pour une ligne de rêve </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Nutrition </h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                    <div class="card-body" style="max-width: 540px; max-height: 240px ">
+                        <h4 class="card-title"> Une méthode simple et efficace pour se motiver </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                    <div class="card-body" style="max-width: 540px; max-height: 240px ">
+                        <h4 class="card-title"> Comment perdre des calories avec efficacité</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Entrainement</h6>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</main>
-
-
+</div>
 <?php
 require_once('includes/footer.php');
 ?>
