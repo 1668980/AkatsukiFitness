68738a583ecd7b9cc4c7fa5f56a70efb937c4619
1668980 <56259470+1668980@users.noreply.github.com>
2022-02-11 16:51:19 -0500

    blog page

--- blog.php
+++ blog.php
@@ -1,60 +1,149 @@
 <?php
 require_once('includes/header.php');
 ?>
-<div class="container mt-5">
-    <div class="card-group container-fluid">
+<main class="bg-dark">
+<div class="container">
+    <div class="card-group container-fluid mt-5">
         <div class="row">
             <div class="col-sm-4">
-                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
                     <div class="card-body">
-                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h4 class="card-title"> Comment sse motiver à prendre les poids </h4>
                         <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
-                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
-                    <div class="card-body" style="max-width: 540px; max-height: 240px ">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body" style="max-width: 540px;  max-height: 240px ">
                         <h4 class="card-title"> Lutter contre les bras flasque</h4>
                         <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Exercice </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
-                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
                     <div class="card-body" style="max-width: 540px; max-height: 240px ">
                         <h4 class="card-title"> Le sport en 2 coup 3 temps </h4>
                         <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Entrainement </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
-                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
                     <div class="card-body" style="max-width: 540px; max-height: 240px ">
                         <h4 class="card-title"> Le parcours à suivre pour une ligne de rêve </h4>
                         <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Nutrition </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
-                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
                     <div class="card-body" style="max-width: 540px; max-height: 240px ">
                         <h4 class="card-title"> Une méthode simple et efficace pour se motiver </h4>
                         <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
-                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; max-height: 240px " href="#">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
                     <div class="card-body" style="max-width: 540px; max-height: 240px ">
                         <h4 class="card-title"> Comment perdre des calories avec efficacité</h4>
                         <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Entrainement</h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3 card-perso card-hover" style="max-width: 540px; min-height:140px ; max-height: 240px " href="blog_article.php">
+                    <div class="card-body">
+                        <h4 class="card-title"> Comment Se motiver à prendre les poids </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">18/08/2021 <br /> Motivation </h6>
+                        <a href="blog_article.php" class="stretched-link" >Lire l'article</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+</main>
 <?php
 require_once('includes/footer.php');
 ?>
--- blog_article.php
+++ blog_article.php
@@ -3,13 +3,42 @@ require_once('includes/header.php');
 ?>
 
 <main class="bg-dark">
-    <div class="container w-50 ">
+    <div class="container">
         <div class="card-group container-fluid ">
-
-            <div class="card align-items-center mt-5 mb-5">
-                show article  <?php echo $_REQUEST['id'] ?>
+            <div class="card mt-3 mb-5">
+                <div class="card-header text-center">
+                    Article
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title text-center ">Comment se motiver à prendre les Pettes</h4>
+                    <div class="m-5">
+                        <p>You don't have to take a course to understand what a low-carb diet is all about. From celeb favourites to some even designed by doctors, low-carb diets have mushroomed all over the health industry. We are now faced with a mind-boggling number of options to choose from -- the Atkin's Diet, High Protein Diet, Stillman Diet, Scarsdale Diet, Hollywood Diet, Ketogenic Diet, Zone Diet and many more.</p>
+                        <p>So what you need to do is figure out a low-carb diet that improves your health without messing up your system.</p><br>
+                        <br>
+                        <h6>What are low-carb diets?</h6>
+                        <p>Low-carbohydrate diets or low-carb diets are dietary programs that restrict carbohydrate consumption usually for weight control. Low carb diets are based on the premise that a diet very low in carbohydrate leads to a reduction in the body's insulin production, resulting in fat and protein stores being used as its main energy source. Low-carbohydrate diets are sometimes recommended for people suffering from obesity, chronic fatigue syndrome and diabetes.</p><br>
+                        <br>
+                        <h6>Why are low-carb diets popular?</h6>
+                        <p>People are attracted to low-carb diets as the weight loss is very drastic. Don't we all love instant results! Lots of Hollywood stars adopt a low-carb diet to drop a dress size for a film or remain thin, but those usually involve drastic cutbacks that may not be healthy or even sustainable in the long run.</p><br>
+                        <br>
+                        <h6>How do I switch to low-carb?</h6>
+                        <p>With all the controversies surrounding low-carb diets and the scores of variation, the first step is to become informed. You need to know how cutting carbohydrates works, what foods have carbohydrates, and how to eat a balanced low-carb diet with sufficient fiber, protein and fats.</p>
+                        <p>Read about various low-carb diets and then zero in on one. Avoid drastic diet plans that permit no vegetables or fruit – cutting out fiber is definitely not healthy and obviously boring! How can long can you eat meat, day in and day out?</p>
+                        <p>Most diets ask you to cut down on carbohydrate in your diet and increase your protein and fat intake. Foods which are high in carbs (e.g. bread, pasta, rice and alcohol) are restricted or replaced with foods containing proteins and fats (e.g., meat, soy products, cheese) and often other foods low in carbohydrates (e.g., green leafy vegetables).</p><br>
+                        <br>
+                        <h6>Avoid white</h6>
+                        <p>Sounds daunting? Don't despair. There are scores of tasty as well as high-fiber and sugar-free snacks and pastas available at a mouse click. (http://www.laurelhealthfoods.com) In fact, the simplest way to go low-carb is by switching to a ‘No White Diet' and ‘No Sugar Diet'. Simply stop eating food made of white flour such as pasta, bread, cake as well as white rice and potatoes. The same goes for sodas and juices loaded with sugar. </p>
+                        <p>But let's not constantly focus on what you can't have – it is totally depressing! Think about all that you can merrily eat! There is a lot sill left on the platter – low-starch vegetables and fruits, meats, dairy products etc. All types of meat as well as eggs are allowed except for liver. You can enjoy seafood like fish and shellfish without guilt pangs! While potatoes and corn may be a no-no, you can still enjoy low-sugar fruits and low-starch leafy veggies!</p><br>
+                        <br>
+                        <h6>Plan it out</h6>
+                        <p>So plan your low-carb week well in advance by deciding on meal menus. Stock up on these foods to avoid giving into carb cravings. Nothing derails a diet faster than realizing you have no low-carb food or snack around when the hunger pangs strike.</p>
+                        <p>You may give in now and then to carb cravings, but don't let it frustrate you! De-carb your way to weight loss!</p>
+                    </div>
+                </div>
+                <div class="card-footer text-muted">
+                    2 days ago
+                </div>
             </div>
-
         </div>
     </div>
 </main>
