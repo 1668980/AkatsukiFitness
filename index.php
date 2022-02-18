057055b2c0df10216bcee2f5fc662026645f118a
Chha Saviet <phangchaly@gmail.com>
2022-02-18 02:16:55 -0500

    new carrousel index

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
+
+$carousel=true;
+
 require_once('includes/header.php');
 ?>
 <!-- Corps de la page -->
-<main class="bg-dark">
-
-    <p style="width:100%; color:WHITE; text-align:center; font-size:500%; font-family:Stencil Std, fantasy; padding-top:2%;">Pourquoi choisir Akatsuki Fitness ?</p>
-
-    <!-- Caroussel faisant défiler différents articles ou entrainemnts (peut être amélioré)-->        
-    <?php require_once ('vue/carrousel.php'); ?>   
-    
+   
     <!-- Container de cartes pour les articles, entrainemnts ou recettes (contenu) -->
     <?php require_once ('vue/plans.php'); ?>
     
@@ -16,6 +13,5 @@ require_once('includes/header.php');
         <button class="btn btn-success" type="button" style="width:100%; font-size:200% ">INSCRIVEZ-VOUS!</button>
 </div>
         
-<main> 
 
 <?php require_once('includes/footer.php');?>
