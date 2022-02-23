6fa39dc9574340d8a007ecb7159ac7ddcd0e338c
OrlandoAriel <orlandovillanueva11@gmail.com>
2022-02-23 18:07:29 -0500

    fix

--- shop_index.php
+++ shop_index.php
@@ -37,22 +37,22 @@ $productList = $crud->getAllProduits();
 ?>
 
 
-<!-- HTML template -->
+<!-- product container -->
 <div id="containerShop" class="container">
     <section class="py-5">
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             <!-- chaque cards -->
             <?php foreach($productList as $products) {?>
             <div class="col mb-5">
-                <div class="card h-100">
+                <div class="card h-100 card-product">
                     <!-- Product image-->
-                    <img class="card-img-top" src="images/products/whey.png" alt="..." />
+                    <img class="card-img-top" src="images/products/<?php echo $products['image']?>" alt="..." />
                     <!-- Product details-->
                     <div class="card-body p-4">
                         <div class="text-center">
                             <!-- Product name-->
-                            <h3 class="fw-bolder"><?php echo $products['nom'] ?></h3>
-                            <h5 class="fw-bolder"><?php echo $products['marque'] ?></h5>
+                            <h4 class="fw-bolder"><?php echo $products['nom'] ?></h4>
+                            <h6 class="fw-bolder"><?php echo $products['marque'] ?></h6>
                             <!-- Product price-->
                             <?php echo $products['info'] ?>
                             <?php echo $products['prix'] ?>
