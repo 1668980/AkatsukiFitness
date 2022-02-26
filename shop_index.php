4b9fae4f31f3c5d725c69c24090bc19b5026d3e4
OrlandoAriel <orlandovillanueva11@gmail.com>
2022-02-25 20:54:24 -0500

    Update shop_index.php

--- shop_index.php
+++ shop_index.php
@@ -41,7 +41,7 @@ if(isset($_GET['idcat'])){
     </div>
 
 
-
+<!-- les resultats -->
     <section class="py-5">
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             <!-- chaque cards -->
