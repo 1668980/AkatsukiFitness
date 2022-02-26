9530aaa2fa7c81054aaf1b6380ee925b01d504d9
RAnas12 <56700993+RAnas12@users.noreply.github.com>
2022-02-26 10:57:22 -0500

    fix default workout

--- db/Entrainement.php
+++ db/Entrainement.php
@@ -2,21 +2,28 @@
 require_once 'db/Exercice.php';
 class Entrainement {
     public $id;
+    public $idUser;
     public $name;
     public $difficulte;
     public $type;
     public $listExercises = array();
     
 
-    public function __construct($id,$name,$difficulte,$type){
+    public function __construct($id,$idUser,$name,$difficulte,$type){
+  
         if($id != 0 ){
             $this->idexercice = $id;
         }else{
             $this->idexercice = 0;
         }
+        
+        $this->idUser = $idUser;
         $this->name = $name;
         $this->difficulte = $difficulte;
         $this->type = $type;
+       
+
+
     }
+
 } 
-?>
