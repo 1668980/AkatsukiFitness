cc48f8e718038f2d5ffe0878efa17438675c341c
OrlandoAriel <orlandovillanueva11@gmail.com>
2022-02-25 20:03:22 -0500

    fixed navbar adaptiveness

--- vue/nav.php
+++ vue/nav.php
@@ -1,5 +1,5 @@
     <!-- pour des la couleur du text blanche : navbar-dark / noir : navbar-light -->
-    <nav id="myNav" class="navbar navbar-expand-md navbar-dark bg-danger bg-gradient">
+    <nav id="myNav" class="navbar navbar-expand-lg navbar-dark bg-danger bg-gradient">
         <div class="container-fluid">
             <a class="logo navbar-brand" href="index.php"><i class="bi bi-check2-circle"> </i>Akatsuki Fitness</a>
             <button id="toggler" class="navbar-toggler" type="button" data-bs-toggle="collapse"
