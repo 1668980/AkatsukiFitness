c9f6544008521f655f717b0cd1d965154b496e68
OrlandoAriel <orlandovillanueva11@gmail.com>
2022-03-01 00:09:15 -0500

    fixed js not responding (jquery)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+$(function(){
+    $('#passInsc').on('keyup', function () {
+        var pattern = /^[A-Za-z\d_-]{8,25}$/;
+        if (pattern.test($('#passInsc').val()) == true) {
+            $('#message1').html('Valide').css('color', 'green');
+        } else
+            $('#message1').html('Non valide').css('color', 'red');
+    });
+    
+    
+    $('#passConf').on('keyup', function () {
+        if ($('#passInsc').val() == $('#passConf').val()) {
+            $('#message2').html('Matching').css('color', 'green');
+        } else
+            $('#message2').html('Not Matching').css('color', 'red');
+    });
+ });
 // sign up functions
 
 validatePassword1 = () => {
@@ -36,22 +53,6 @@ f1 = () => {
         return false;
     }
 }
-
-$('#passInsc').on('keyup', function () {
-    var pattern = /^[A-Za-z\d_-]{8,25}$/;
-    if (pattern.test($('#passInsc').val()) == true) {
-        $('#message1').html('Valide').css('color', 'green');
-    } else
-        $('#message1').html('Non valide').css('color', 'red');
-});
-
-
-$('#passConf').on('keyup', function () {
-    if ($('#passInsc').val() == $('#passConf').val()) {
-        $('#message2').html('Matching').css('color', 'green');
-    } else
-        $('#message2').html('Not Matching').css('color', 'red');
-});
 
 
 var toastTrainingOptions = document.getElementById('toastTrainingOptions')
