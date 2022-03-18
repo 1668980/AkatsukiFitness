25db5e16aa8c0b42e8d7fbe56572dcbd23651df2
Orlando Ariel <orlandovillanueva11@gmail.com>
2022-03-18 16:20:31 -0400

    update ex UI improvement

--- update_exercice.php
+++ update_exercice.php
@@ -3,31 +3,34 @@ require_once 'includes/header.php';
 if(isset($_POST['idExercice'])){
 
 }?>
-<form action="update_exercice_action.php" method="POST">
-<div class="text-light">
-    <input hidden name="idEntrainement" value="<?php echo $_POST['idEntrainement']?>">
-    <input hidden name="idExercice" value="<?php echo $_POST['idExercice']?>">
-    <div>   
-        <label for="poids">poids:</label>
-        <input name="poids" value="<?php echo $_POST['poids'] ?>" >
-    </div>
-    <div> 
-        <label for="reps">repetition:</label>
-        <input name="reps" value="<?php echo $_POST['reps'] ?>" >
-    </div><div> 
-        <label for="sets">sets:</label>
-        <input name="sets" value="<?php echo $_POST['sets'] ?>" >
-    </div><div> 
-        <label for="duree">duree:</label>
-        <input name="duree" value="<?php echo $_POST['duree'] ?>" >
-    </div><div> 
-        <label for="dureePause">dureepause:</label>
-        <input name="dureePause" value="<?php echo $_POST['dureepause'] ?>" >
-    </div>
+<div id="contUpdateEx" class=" container mt-3">
+    <form action="update_exercice_action.php" method="POST">
+        <div class="text-light">
+            <input hidden name="idEntrainement" value="<?php echo $_POST['idEntrainement']?>">
+            <input hidden name="idExercice" value="<?php echo $_POST['idExercice']?>">
+            <div>
+                <label  class="form-label" for="poids">poids:</label>
+                <input class="form-control" name="poids" value="<?php echo $_POST['poids'] ?>">
+            </div>
+            <div>
+                <label  class="form-label mt-1" for="reps">repetition:</label>
+                <input class="form-control" name="reps" value="<?php echo $_POST['reps'] ?>">
+            </div>
+            <div>
+                <label  class="form-label mt-1" for="sets">sets:</label>
+                <input class="form-control" name="sets" value="<?php echo $_POST['sets'] ?>">
+            </div>
+            <div>
+                <label  class="form-label mt-1" for="duree">duree:</label>
+                <input class="form-control" name="duree" value="<?php echo $_POST['duree'] ?>">
+            </div>
+            <div>
+                <label  class="form-label mt-1" for="dureePause">dureepause:</label>
+                <input class="form-control" name="dureePause" value="<?php echo $_POST['dureepause'] ?>">
+            </div>
 
 
+        </div>
+        <button class="btn btn-success mt-2" type="sumbit">Modifier</button>
+    </form>
 </div>
-<button type ="sumbit">Modifier</button>
-</form>
-      
-
